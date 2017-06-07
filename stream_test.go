9950c0003fa6d36790b756db8a65797ed8a6f969
Marten Seemann <martenseemann@gmail.com>
2017-06-07 13:07:53 +0200

    make utils an internal package

--- stream_test.go
+++ stream_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/lucas-clemente/quic-go/flowcontrol"
 	"github.com/lucas-clemente/quic-go/frames"
 	"github.com/lucas-clemente/quic-go/protocol"
-	"github.com/lucas-clemente/quic-go/utils"
+	"github.com/lucas-clemente/quic-go/internal/utils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
